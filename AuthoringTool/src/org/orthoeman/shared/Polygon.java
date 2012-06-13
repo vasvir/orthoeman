081e4890a6cf110fd393575071a5094f6a3cad28
Vassilis Virvilis <vasvir2@gmail.com>
2012-06-13 20:13:51 +0300

    add some meat to Polygon hotspot

--- AuthoringTool/src/org/orthoeman/shared/Polygon.java
+++ AuthoringTool/src/org/orthoeman/shared/Polygon.java
@@ -1,4 +1,15 @@
 package org.orthoeman.shared;
 
+import java.util.List;
+
 public class Polygon extends Drawing {
+	private List<Point> points;
+
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
 }
