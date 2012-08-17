beea56e1882b0e625499cabf49262854ce5384ab
Vassilis Virvilis <vasvir2@gmail.com>
2012-08-17 22:09:25 +0300

    testing push of moodle plugin

--- moodle/orthoeman/version.php
+++ moodle/orthoeman/version.php
@@ -1,4 +1,4 @@
 <?php
-	$plugin->version = 2012081701;  // YYYYMMDDHH (year, month, day, 24-hr time)
+	$plugin->version = 2012081702;  // YYYYMMDDHH (year, month, day, 24-hr time)
 	$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 ?>
