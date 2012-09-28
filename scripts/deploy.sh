511cfbb3247774c1c38fd39effbd742acc545688
Vassilis Virvilis <vasvir2@gmail.com>
2012-09-28 20:41:40 +0300

    added deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+
+scriptdir=`dirname $0`;
+plugindir="$scriptdir/../moodle/orthoeman";
+ls -ltd $plugindir;
+ls $plugindir;
+rsync -av "$plugindir" root@orthoeman:/usr/share/moodle/mod/;
+ssh root@orthoeman '
+	chown root.www-data /usr/share/moodle/mod/orthoeman/db/install.xml /usr/share/moodle/mod/orthoeman/db;
+	chmod 664 /usr/share/moodle/mod/orthoeman/db/install.xml;
+	chmod 775 /usr/share/moodle/mod/orthoeman/db;
+';
