7ef0d575e3c84e2d26dbd4ce2cdd0d3ff6008d52
Vassilis Virvilis <vasvir2@gmail.com>
2012-10-24 22:07:49 +0300

    block GUI while converting video...

---
+++ AuthoringTool/src/org/orthoeman/client/ProgressDialogBox.java
@@ -0,0 +1,36 @@
+package org.orthoeman.client;
+
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class ProgressDialogBox extends DialogBox {
+    public ProgressDialogBox(String text) {
+        setText(text != null ? text : "Retrieving Data...");
+        center();
+    }
+
+    public ProgressDialogBox() {
+    	this(null);
+    }
+
+    	@Override
+    public void show() {
+        showWaitCursor();
+        super.show();
+    }
+
+    @Override
+    public void hide() {
+        super.hide();
+        showDefaultCursor();
+    }
+
+    public static void showWaitCursor() {
+        DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor", "wait");
+    }
+
+    public static void showDefaultCursor() {
+        DOM.setStyleAttribute(RootPanel.getBodyElement(), "cursor", "default");
+    }
+}
