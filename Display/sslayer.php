f1f40e5cfa59fbe7da0e0104235a6b3f431cb1d9
Konstantinos Zagoris <kzagoris@gmail.com>
2012-11-03 19:18:21 +0200

    debuging

--- Display/sslayer.php
+++ Display/sslayer.php
@@ -4,7 +4,7 @@ session_start();
 require_once('fb.php');
 //echo dirname(dirname(dirname('../lib.php'))).'/config.php';
 //require_once(dirname(dirname(dirname('../lib.php'))).'/config.php');
-require_once('../../../../config.php');
+require_once('../../../config.php');
 echo dirname('../lib.php').'/lib.php';
 require_once(dirname('../lib.php').'/lib.php');
 
