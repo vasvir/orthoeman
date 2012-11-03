42219165c7b8bee6551c40f3a2ba60a7a8b0c05d
Konstantinos Zagoris <kzagoris@gmail.com>
2012-11-03 19:43:54 +0200

    debug

--- moodle/orthoeman/sslayer.php
+++ moodle/orthoeman/sslayer.php
@@ -1,7 +1,7 @@
 <?php
  ob_start();
 //session_start();
-require_once('fb.php');
+require_once('Display/fb.php');
 //echo dirname(dirname(dirname('../lib.php'))).'/config.php';
 //require_once(dirname(dirname(dirname('../lib.php'))).'/config.php');
 require_once('../../config.php');
