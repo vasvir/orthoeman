97a9f58d09ca54c24adf335ea8de54fa2c4d82ad
Konstantinos Zagoris <kzagoris@gmail.com>
2012-11-03 18:18:28 +0200

    try to find what is wrong

--- Display/sslayer.php
+++ Display/sslayer.php
@@ -356,6 +356,8 @@ function getXMLData(){
 	require_once('../lib.php');
 	$orthoeman_id = isset($_GET['orthoeman_id'])? (int)$_GET['orthoeman_id'] : -1;
 	$resource = get_database_data($orthoeman_id,-1);
+	echo $orthoeman_id;
+	echo $resource;
 	//return simplexml_load_file(filename);
 	return $resource->data;
 }
