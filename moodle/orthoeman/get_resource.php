5b761775c3a5ad05da6383a82d95b6b9b5f75c3f
Vassilis Virvilis <vasvir2@gmail.com>
2012-11-18 22:59:41 +0200

    rename byte_serve.php to byteserve.php

--- moodle/orthoeman/get_resource.php
+++ moodle/orthoeman/get_resource.php
@@ -31,7 +31,7 @@
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
-require_once(dirname(__FILE__).'/byte_serve.php');
+require_once(dirname(__FILE__).'/byteserve.php');
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
 $n  = optional_param('n', 0, PARAM_INT);  // orthoeman instance ID - it should be named as the first character of the module
