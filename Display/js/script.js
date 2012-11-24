722732252632eb8f63aa08e68dee3aa093355d88
Konstantinos Zagoris <kzagoris@gmail.com>
2012-11-24 13:01:04 +0200

    initial support for timeout timer

--- Display/js/script.js
+++ Display/js/script.js
@@ -144,6 +144,10 @@ $(document).ready(function () {
             }
         }
 
+        $('#counter').countdown({
+            timestamp : (new Date()).getTime() + 51*24*60*60*1000
+        });
+
 
     });
 });
