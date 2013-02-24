a14e871b3dd9faf4145a8ce49e39d0b03a38eccc
Vassilis Virvilis <vasvir2@gmail.com>
2013-02-24 14:01:44 +0200

    add the capability to delete answers

---
+++ moodle/orthoeman/delete_answers.php
@@ -0,0 +1,70 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Prints a particular instance of orthoeman
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage orthoeman
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/// (Replace orthoeman with the name of your module and remove this line)
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+
+$id = optional_param('id', 0, PARAM_INT); // course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // orthoeman instance ID - it should be named as the first character of the module
+
+if ($id) {
+    $cm         = get_coursemodule_from_id('orthoeman', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $orthoeman  = $DB->get_record('orthoeman', array('id' => $cm->instance), '*', MUST_EXIST);
+} elseif ($n) {
+    $orthoeman  = $DB->get_record('orthoeman', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $orthoeman->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('orthoeman', $orthoeman->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+
+$user_id = optional_param('user_id', -1, PARAM_INT);
+$page_id = optional_param('page_id', -1, PARAM_INT);
+
+require_login($course, true, $cm);
+$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+
+require_capability('mod/orthoeman:write', $context);
+
+add_to_log($course->id, 'orthoeman', 'put_resource', "put_answer.php?id={$cm->id}", $orthoeman->name, $cm->id);
+
+$match_array = array('orthoeman_id' => $id);
+
+if ($user_id >= 0) {
+    $match_arrray['user_id'] = $user_id;
+}
+
+if ($page_id >= 0) {
+    $match_array['page_id'] = $page_id;
+}
+
+$DB->delete_records($ANSWER_TABLE, $match_array);
