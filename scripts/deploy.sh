c163c65fda94b4e686bf4ef968a9503c6eeba81d
Vassilis Virvilis <vasvir2@gmail.com>
2013-08-15 01:01:04 +0300

    deprecated Content
    Former-commit-id: 8d01de2c44f70d29985701b9d7951fdf959d8a3c

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,13 @@ RSYNC="rsync -avz --delete --exclude '*~'";
 
 scriptdir=`dirname $0`;
 rootdir="$scriptdir/..";
-plugindir="$rootdir/moodle/orthoeman";
+plugindir="$rootdir/orthoeman";
 authoringtooldir="$rootdir/AuthoringTool/war/";
 displaydir="$rootdir/Display";
-contentdir="$rootdir/Content";
 wwwdir="$rootdir/www";
 
 $RSYNC --exclude AuthoringTool --exclude Display "$plugindir" www-data@orthoeman:/usr/share/moodle/mod;
 $RSYNC --exclude WEB-INF "$authoringtooldir" www-data@orthoeman:/usr/share/moodle/mod/orthoeman/AuthoringTool;
 #$RSYNC "$displaydir" www-data@orthoeman:/usr/share/moodle/mod/orthoeman;
 $RSYNC "$wwwdir" www-data@orthoeman:/var;
-$RSYNC "$contentdir/XML/orthoeman.xsd" www-data@orthoeman:/var/www;
+$RSYNC "$plugindir/orthoeman.xsd" www-data@orthoeman:/var/www;
