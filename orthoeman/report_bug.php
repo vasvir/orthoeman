3b74149d9e053e830a02a439da08c385bf06d7d9
Vassilis Virvilis <vasvir2@gmail.com>
2013-08-20 22:01:35 +0300

    split report_bug to library and script parts
    Former-commit-id: 51ee4f882ed7493a666171e8cc84b58d7ad399e1

--- orthoeman/report_bug.php
+++ orthoeman/report_bug.php
@@ -16,14 +16,16 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Prints a particular instance of orthoeman
+ * Sends a bug report to the admin of the moodle installation
  *
- * You can have a rather longer description of the file as well,
- * if you like, and it can span multiple lines.
+ * The bug report consists of a user part and a dump of moodle infomration.
+ * The subject is optional in the url.
+ * The user part of the body must be transmited with a POST http request
+ *
  *
  * @package    mod
  * @subpackage orthoeman
- * @copyright  2011 Your Name
+ * @copyright  Vassilis Virvilis
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -34,62 +36,20 @@ require_once(dirname(__FILE__).'/lib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
 $n  = optional_param('n', 0, PARAM_INT);  // orthoeman instance ID - it should be named as the first character of the module
-
-if ($id) {
-    $cm         = get_coursemodule_from_id('orthoeman', $id, 0, false, MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
-    $orthoeman  = $DB->get_record('orthoeman', array('id' => $cm->instance), '*', MUST_EXIST);
-} elseif ($n) {
-    $orthoeman  = $DB->get_record('orthoeman', array('id' => $n), '*', MUST_EXIST);
-    $course     = $DB->get_record('course', array('id' => $orthoeman->course), '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('orthoeman', $orthoeman->id, $course->id, false, MUST_EXIST);
-} else {
-    error('You must specify a course_module ID or an instance ID');
-}
-
 $subject = optional_param('subject', '', PARAM_TEXT);
 
-require_login($course, true, $cm);
-$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+list($course, $cm, $orthoeman, $context) = get_moodle_data($id, $n);
 
-require_capability('mod/orthoeman:write', $context);
-
-add_to_log($course->id, 'orthoeman', 'report_bug', "report_bug?id={$cm->id}", $orthoeman->name, $cm->id);
-
-
-global $USER;
+header("Content-type: text/plain");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    header("Content-type: text/plain");
-
     $text = urldecode(file_get_contents('php://input'));
-    $moodle_info = "Username: $USER->username
-Name: $USER->firstname $USER->lastname
-e-mail: $USER->email
-skype: $USER->skype
-url:$USER->url
-Institution: $USER->institution
-Department: $USER->department
-Address: $USER->address
-City: $USER->city
-Country: $USER->country\ncm: " . print_r($cm, true) . "\ncourse: " . print_r($course, true) . "\northoeman: " . print_r($orthoeman, true) . "\nuser: " . print_r($USER, true);;
-    $body = "$text\n\nMoodle Information\n\n$moodle_info\n";
-
-    //echo $body;
-
-    $to_user = get_admin();
-    $subject = "[BUG: $course->shortname] $USER->firstname $USER->lastname: $subject";
-    $success = email_to_user($to_user, $USER, $subject, $body);
-    if ($success == "1") {  
-        echo "E-mail successful sent to $to_user->firstname $to_user->lastname!";
-    } else if ($success === "emailstop") {
-        echo "E-mail of $to_user->firstname $to_user->lastname has been DISABLED! E-mail NOT sent";
-        http_response_code(406);
-    } else if (!$success) {
-        echo "Error: E-mail unsuccessful.";
-        http_response_code(404);
-    }
+    $result = report_bug($course, $cm, $orthoeman, $context, $subject, $text);
 } else {
-    echo "Invalid use of orthoeman plugin resource";
-    http_response_code(403);
+    $result = new stdClass();
+    $result->msg = "Invalid use of orthoeman plugin resource";
+    $result->status = 403; 
 }
+            
+echo($result->msg);
+http_response_code($result->status);
