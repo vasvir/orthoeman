ed012196b7dd372c5290e2d1680f1fe0bb904a1c
Vassilis Virvilis <vasvir2@gmail.com>
2013-08-24 20:31:45 +0300

    fix deploy scripts
    Former-commit-id: 47f94713aaf0aee37bb4dc033b893493b69be0b2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,13 @@ RSYNC="rsync -avz --delete --exclude '*~'";
 scriptdir=`dirname $0`;
 rootdir="$scriptdir/..";
 plugindir="$rootdir/orthoeman";
-authoringtooldir="$rootdir/AuthoringTool/war/";
-displaydir="$rootdir/Display";
+authoringtooldir="$plugindir/AuthoringTool/war/";
+displaydir="$plugindir/Display";
 wwwdir="$rootdir/www";
 
-$RSYNC --exclude AuthoringTool --exclude Display "$plugindir" www-data@orthoeman:/usr/share/moodle/mod;
+EXCLUDE_DISPLAY='--exclude Display';
+
+$RSYNC --exclude AuthoringTool $EXCLUDE_DISPLAY "$plugindir" www-data@orthoeman:/usr/share/moodle/mod;
 $RSYNC --exclude WEB-INF "$authoringtooldir" www-data@orthoeman:/usr/share/moodle/mod/orthoeman/AuthoringTool;
-#$RSYNC "$displaydir" www-data@orthoeman:/usr/share/moodle/mod/orthoeman;
 $RSYNC "$wwwdir" www-data@orthoeman:/var;
 $RSYNC "$plugindir/orthoeman.xsd" www-data@orthoeman:/var/www;
