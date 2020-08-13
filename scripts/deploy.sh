7a541ab2ce23519e876452448046bbd4c02b6de0
Vassilis Virvilis <vasvir2@gmail.com>
2020-08-13 20:40:23 +0300

    modernize deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -e
 
 RSYNC="rsync -avz --delete --exclude '*~'";
+REMOTE_USER=root;
+REMOTE_HOST=yamato;
+REMOTE="$REMOTE_USER@$REMOTE_HOST";
+REMOTE_DIR='/var/www/moodle.e-trauma.org/mod';
 
 scriptdir=`dirname $0`;
 rootdir="$scriptdir/..";
@@ -11,7 +15,7 @@ wwwdir="$rootdir/www";
 
 EXCLUDE_DISPLAY='--exclude Display';
 
-$RSYNC --exclude AuthoringTool $EXCLUDE_DISPLAY "$plugindir" www-data@orthoeman:/usr/share/moodle/mod;
-$RSYNC --exclude WEB-INF "$authoringtooldir" www-data@orthoeman:/usr/share/moodle/mod/orthoeman/AuthoringTool;
-$RSYNC "$wwwdir" www-data@orthoeman:/var;
-$RSYNC "$plugindir/orthoeman.xsd" www-data@orthoeman:/var/www;
+$RSYNC --exclude AuthoringTool $EXCLUDE_DISPLAY "$plugindir" $REMOTE:$REMOTE_DIR;
+#echo $RSYNC --exclude WEB-INF "$authoringtooldir" $REMOTE:$REMOTE_DIR/orthoeman/AuthoringTool;
+#echo $RSYNC "$wwwdir" $REMOTE:/var;
+#echo $RSYNC "$plugindir/orthoeman.xsd" $REMOTE:/var/www;
