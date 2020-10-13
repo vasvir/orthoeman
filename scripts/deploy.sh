58bcc471cd491fd1c882d0dd9ed3c2c17ebab601
Vassilis Virvilis <vasvir2@gmail.com>
2020-10-14 01:16:46 +0300

    fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,16 @@ RSYNC="rsync -avz --delete --exclude '*~'";
 REMOTE_USER=root;
 REMOTE_HOST=yamato;
 REMOTE="$REMOTE_USER@$REMOTE_HOST";
-REMOTE_DIR='/var/www/moodle.e-trauma.org/mod';
+REMOTE_DIR='/var/www/moodle.e-trauma.org/mod/orthoeman';
 
 scriptdir=`dirname $0`;
 rootdir="$scriptdir/..";
-plugindir="$rootdir/orthoeman";
+plugindir="$rootdir";
 authoringtooldir="$plugindir/AuthoringTool/war/";
-displaydir="$plugindir/Display";
+
+$RSYNC --exclude AuthoringTool --exclude scripts --exclude .git --exclude .gitignore --exclude doc "$plugindir" "$REMOTE:$REMOTE_DIR";
+$RSYNC --exclude WEB-INF "$authoringtooldir" "$REMOTE:$REMOTE_DIR/AuthoringTool";
+
 wwwdir="$rootdir/www";
-
-EXCLUDE_DISPLAY='--exclude Display';
-
-$RSYNC --exclude AuthoringTool $EXCLUDE_DISPLAY "$plugindir" $REMOTE:$REMOTE_DIR;
-$RSYNC --exclude WEB-INF "$authoringtooldir" $REMOTE:$REMOTE_DIR/orthoeman/AuthoringTool;
 #echo $RSYNC "$wwwdir" $REMOTE:/var;
 #echo $RSYNC "$plugindir/orthoeman.xsd" $REMOTE:/var/www;
