e2ff0f4a771e523006fc2e1b5f9904c187ad3353
Vassilis Virvilis <vasvir2@gmail.com>
2020-10-14 01:26:29 +0300

    added some todos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,13 +7,16 @@ REMOTE="$REMOTE_USER@$REMOTE_HOST";
 REMOTE_DIR='/var/www/moodle.e-trauma.org/mod/orthoeman';
 
 scriptdir=`dirname $0`;
-rootdir="$scriptdir/..";
-plugindir="$rootdir";
+plugindir="$scriptdir/..";
 authoringtooldir="$plugindir/AuthoringTool/war/";
 
 $RSYNC --exclude AuthoringTool --exclude scripts --exclude .git --exclude .gitignore --exclude doc "$plugindir" "$REMOTE:$REMOTE_DIR";
 $RSYNC --exclude WEB-INF "$authoringtooldir" "$REMOTE:$REMOTE_DIR/AuthoringTool";
 
-wwwdir="$rootdir/www";
+#TODO: what will happen to orthoeman.xsd
+#TODO: documentation deployment
+#TODO: release/zip creation
+
+#wwwdir="$rootdir/www";
 #echo $RSYNC "$wwwdir" $REMOTE:/var;
 #echo $RSYNC "$plugindir/orthoeman.xsd" $REMOTE:/var/www;
